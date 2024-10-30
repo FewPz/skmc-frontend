51d71383771ac1bae72bc2a1eb11d6bc60fa67c2
Phoz07 <115572920+Phoz07@users.noreply.github.com>
2024-10-30 22:34:50 +0700

    feat: Add utility functions, custom fonts, and Tailwind CSS configuration for improved styling

---
+++ apps/frontend/src/app/fonts.tsx
@@ -0,0 +1,6 @@
+export const minecraftTen = {
+  fontFamily: "Minecraft Ten",
+  src: `url('/fonts/Minecraft/MinecraftTen-VGORe.ttf') format('truetype')`,
+  fontWeight: "normal",
+  fontStyle: "normal",
+};
