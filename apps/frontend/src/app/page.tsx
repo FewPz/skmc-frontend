1ce563ea9cd0976a52f9eb34bb8dd162feb67ecd
FewPz <sharpkung4826@gmail.com>
2024-10-31 23:06:14 +0700

    feat: Add Footer component and update layout with footer integration; simplify main page content

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,12 +1,5 @@
 export default function Page() {
   return (
-    <div>
-      <div className="font-minecraft text-4xl font-medium">
-        Test Minecraft Ten Font (Smilekrub Server)
-      </div>
-      <div className="text-3xl">
-        สมายครับเซิร์ฟเวอร์ (Smilekrub Server)
-      </div>
-    </div>
+    <p>This is main page na kub</p>
   );
 }
