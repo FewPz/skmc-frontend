009d70ae77850fe716229def25185c9b1f50fd85
FewPz <sharpkung4826@gmail.com>
2024-11-02 13:36:23 +0700

    feat: Integrate framer-motion for animations in Hero component; enhance layout and responsiveness

--- apps/frontend/src/components/Hero.tsx
+++ apps/frontend/src/components/Hero.tsx
@@ -1,33 +1,48 @@
+"use client";
+
 import Container from "@/components/Container";
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import CopyButton from "@/components/CopyButton";
+import { motion } from "framer-motion";
 import { Button } from "@/components/ui/button";
 
 export default function Hero() {
   return (
-    <header className="bg-[url(/assets/images/hero_bg.png)] bg-no-repeat bg-cover bg-center min-h-dvh relative">
-      <div className="absolute inset-0 bg-black opacity-40"></div>
+    <header className="bg-[url(/assets/images/hero_bg.png)] bg-no-repeat bg-cover bg-center min-h-screen relative">
+      <div className="absolute inset-0 bg-black opacity-50"></div>
       <Container>
         <Navbar />
-        <div className="absolute inset-0 top-10 flex flex-col items-center justify-center gap-y-8">
-          <div className="w-full max-w-[500px]">
+        <div className="absolute inset-0 top-10 flex flex-col items-center justify-center gap-y-8 px-4 md:px-8 lg:px-0">
+          <div className="w-full max-w-[80%] sm:max-w-[500px] md:max-w-[600px]">
+          <motion.div
+            initial={{ y: 0 }}
+            animate={{ y: [0, -10, 0] }}
+            transition={{
+              duration: 1,
+              ease: "easeInOut",
+              repeat: Infinity,
+              repeatType: "loop",
+            }}
+          >
             <Image
-              src={"/assets/images/smilekrub.png"}
+              src="/assets/images/smilekrub.png"
               alt="smilekrub"
               width={600}
               height={100}
+              className="w-full h-auto"
             />
+          </motion.div>
           </div>
-          <div>
+          <div className="text-center">
             <CopyButton label="play.sk-mc.net" text="play.sk-mc.net" />
           </div>
-          <div className="space-x-4">
-            <Button variant="default" size="lg" className="font-mc">
-              login
+          <div className="space-x-4 flex flex-wrap justify-center">
+            <Button variant="default" size="lg" className="font-mc px-6 md:px-8">
+              Login
             </Button>
-            <Button variant="outline" size="lg" className="font-mc">
-              learn more
+            <Button variant="outline" size="lg" className="font-mc px-6 md:px-8">
+              Learn More
             </Button>
           </div>
         </div>
