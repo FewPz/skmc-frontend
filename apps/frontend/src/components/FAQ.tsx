21b5429785074505291c23870a1f8060e9065f2c
Phoz07 <115572920+Phoz07@users.noreply.github.com>
2024-11-04 01:59:13 +0700

    feat: Add FAQ section with accordion component;

--- apps/frontend/src/components/FAQ.tsx
+++ apps/frontend/src/components/FAQ.tsx
@@ -1,12 +1,49 @@
 import Title from "@/components/ui/title";
+import Container from "@/components/Container";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 export default function FAQ() {
   return (
-    <div
-      id="faq"
-      className="flex justify-center relative bg-white bg-no-repeat bg-cover bg-center min-h-dvh"
-    >
-      <Title text="FAQ" color="black" />
-    </div>
+    <section id="faq">
+      <Container>
+        <div
+          id="faq"
+          className="flex justify-center relative bg-white bg-no-repeat bg-cover bg-center min-h-dvh"
+        >
+          <Title text="FAQ" color="black" />
+        </div>
+        <Accordion type="single" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </Container>
+    </section>
   );
 }
