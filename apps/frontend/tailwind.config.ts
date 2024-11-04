e683a9548006aca5b8458005b37fb76ec17e5671
FewPz <sharpkung4826@gmail.com>
2024-11-04 12:56:28 +0700

    fix: Standardize code formatting in accordion component and tailwind config; improve readability

--- apps/frontend/tailwind.config.ts
+++ apps/frontend/tailwind.config.ts
@@ -8,55 +8,55 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	screens: {
-  		sm: '640px',
-  		md: '768px',
-  		lg: '1024px',
-  		xl: '1280px'
-  	},
-  	container: {
-  		center: 'true',
-  		padding: '1rem'
-  	},
-  	extend: {
-  		colors: {
-  			primary: '#52D974',
-  			secondary: '#3FA659',
-  			darkGreen: '#27733A',
-  			customWhite: '#F3F3F3',
-  			customBlack: '#0D0D0D',
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			border: 'hsl(var(--border))'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		keyframes: {
-  			'accordion-down': {
-  				from: {
-  					height: '0'
-  				},
-  				to: {
-  					height: 'var(--radix-accordion-content-height)'
-  				}
-  			},
-  			'accordion-up': {
-  				from: {
-  					height: 'var(--radix-accordion-content-height)'
-  				},
-  				to: {
-  					height: '0'
-  				}
-  			}
-  		},
-  		animation: {
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out'
-  		}
-  	}
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    container: {
+      center: "true",
+      padding: "1rem",
+    },
+    extend: {
+      colors: {
+        primary: "#52D974",
+        secondary: "#3FA659",
+        darkGreen: "#27733A",
+        customWhite: "#F3F3F3",
+        customBlack: "#0D0D0D",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        border: "hsl(var(--border))",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 };
