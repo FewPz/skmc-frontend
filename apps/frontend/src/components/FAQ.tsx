befcad1fefc0e9a83af7a188f6d853e7adce84eb
Phoz07 <115572920+Phoz07@users.noreply.github.com>
2024-11-05 15:04:47 +0700

    feat: refactor FAQ component to use dynamic questions and improve accordion styles

--- apps/frontend/src/components/FAQ.tsx
+++ apps/frontend/src/components/FAQ.tsx
@@ -6,41 +6,24 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { questions } from "@/constants/faq";
 
 export default function FAQ() {
   return (
     <section id="faq" className="min-h-dvh">
       <Container>
-        <div className="space-y-6">
+        <div>
           <Title>FAQ</Title>
           <div>
-            <Accordion type="multiple">
-              <AccordionItem value="item-1">
-                <AccordionTrigger>
-                  เซิร์ฟเวอร์ Minecraft คืออะไร
-                </AccordionTrigger>
-                <AccordionContent>
-                  Yes. It adheres to the WAI-ARIA design pattern.
-                </AccordionContent>
-              </AccordionItem>
-              <AccordionItem value="item-2">
-                <AccordionTrigger>เซิร์ฟเวอร์สมายครับคืออะไร</AccordionTrigger>
-                <AccordionContent>
-                  Yes. It adheres to the WAI-ARIA design pattern.
-                </AccordionContent>
-              </AccordionItem>
-              <AccordionItem value="item-3">
-                <AccordionTrigger>ทำไมสมายครับไม่เหมือนเดิม</AccordionTrigger>
-                <AccordionContent>
-                  Yes. It adheres to the WAI-ARIA design pattern.
-                </AccordionContent>
-              </AccordionItem>
-              <AccordionItem value="item-4">
-                <AccordionTrigger>Is it accessible?</AccordionTrigger>
-                <AccordionContent>
-                  Yes. It adheres to the WAI-ARIA design pattern.
-                </AccordionContent>
-              </AccordionItem>
+            <Accordion type="single" collapsible>
+              {questions.map((q, index) => (
+                <AccordionItem value={String(index)}>
+                  <AccordionTrigger>{q.header}</AccordionTrigger>
+                  <AccordionContent>
+                    <p className="p-4 text-sm">{q.content}</p>
+                  </AccordionContent>
+                </AccordionItem>
+              ))}
             </Accordion>
           </div>
         </div>
---
+++ apps/frontend/src/constants/faq.ts
@@ -0,0 +1,18 @@
+export const questions = [
+  {
+    header: "เซิร์ฟเวอร์ Minecraft คืออะไร?",
+    content: "Sample content",
+  },
+  {
+    header: "เซิร์ฟเวอร์สมายครับคืออะไร?",
+    content: "Sample content",
+  },
+  {
+    header: "ทำไมสมายครับไม่เหมือนเดิม?",
+    content: "Sample content",
+  },
+  {
+    header: "คำถามที่ 4",
+    content: "Sample content",
+  },
+];
