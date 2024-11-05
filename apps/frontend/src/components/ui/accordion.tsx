1715839790c7e61406fdda5ba9b2b60b4595334a
DucKyWay <80985006+DucKyWay@users.noreply.github.com>
2024-11-05 14:16:11 +0700

    feat: add color and text font accordion faq

--- apps/frontend/src/components/ui/accordion.tsx
+++ apps/frontend/src/components/ui/accordion.tsx
@@ -28,7 +28,7 @@ const AccordionTrigger = React.forwardRef<
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        "flex flex-1 items-center justify-between py-4 font-medium text-black transition-all hover:underline [&[data-state=open]>svg]:rotate-18",
+        "flex flex-1 items-center justify-between mb-5 py-3 pl-8 pr-10 font-bold text-black bg-[#52D974] transition-all hover:underline [&[data-state=open]>svg]:rotate-18",
         className,
       )}
       {...props}
@@ -46,10 +46,10 @@ const AccordionContent = React.forwardRef<
 >(({ className, children, ...props }, ref) => (
   <AccordionPrimitive.Content
     ref={ref}
-    className="overflow-hidden text-sm text-black transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
+    className="overflow-hidden px-8 text-sm text-black transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
     {...props}
   >
-    <div className={cn("pb-4 pt-0", className)}>{children}</div>
+    <div className={cn("pb-20 pt-0", className)}>{children}</div>
   </AccordionPrimitive.Content>
 ));
 
