8c7f999389152771f4fb81d954829a0a037290e0
DucKyWay <80985006+DucKyWay@users.noreply.github.com>
2024-11-05 13:57:40 +0700

    feat:add multiple accordion in faq and add question

--- apps/frontend/src/components/FAQ.tsx
+++ apps/frontend/src/components/FAQ.tsx
@@ -9,34 +9,34 @@ import {
 
 export default function FAQ() {
   return (
-    <section id="faq">
+    <section id="faq" className="min-h-dvh">
       <Container>
         <div
           id="faq"
-          className="flex justify-center relative bg-white bg-no-repeat bg-cover bg-center min-h-dvh"
+          className="flex justify-center relative bg-white bg-no-repeat bg-cover bg-center pb-12"
         >
           <Title text="FAQ" color="black" />
         </div>
-        <Accordion type="single" collapsible>
+        <Accordion type="multiple" collapsible>
           <AccordionItem value="item-1">
-            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionTrigger>เซิร์ฟเวอร์ Minecraft คืออะไร</AccordionTrigger>
             <AccordionContent>
               Yes. It adheres to the WAI-ARIA design pattern.
             </AccordionContent>
           </AccordionItem>
-          <AccordionItem value="item-1">
-            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionItem value="item-2">
+            <AccordionTrigger>เซิร์ฟเวอร์สมายครับคืออะไร</AccordionTrigger>
             <AccordionContent>
               Yes. It adheres to the WAI-ARIA design pattern.
             </AccordionContent>
           </AccordionItem>
-          <AccordionItem value="item-1">
-            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+          <AccordionItem value="item-3">
+            <AccordionTrigger>ทำไมสมายครับไม่เหมือนเดิม</AccordionTrigger>
             <AccordionContent>
               Yes. It adheres to the WAI-ARIA design pattern.
             </AccordionContent>
           </AccordionItem>
-          <AccordionItem value="item-1">
+          <AccordionItem value="item-4">
             <AccordionTrigger>Is it accessible?</AccordionTrigger>
             <AccordionContent>
               Yes. It adheres to the WAI-ARIA design pattern.
